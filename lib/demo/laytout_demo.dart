765d5823e30f900152c3d304fb334124324444b1
IlhamTahir <yantaq@bilig.biz>
2018-12-10 21:00:50 +0800

    Stack

--- lib/demo/laytout_demo.dart
+++ lib/demo/laytout_demo.dart
@@ -9,31 +9,66 @@ class LayoutDemo extends StatelessWidget
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          SizedBox(
-            width: 200.0,
-            height: 300.0,
-            child: Container(
-              alignment: Alignment.bottomRight,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0)
+          Stack(
+            alignment: Alignment.topLeft,
+            children: <Widget>[
+              SizedBox(
+                width: 200.0,
+                height: 300.0,
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(3, 54, 255, 1.0),
+                      borderRadius: BorderRadius.circular(8.0)
+                  ),
+                ),
               ),
-              child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0),
-            ),
-          ),
-          SizedBox(
-            height: 32.0,
-          ),
-          SizedBox(
-            width: 100.0,
-            height: 100.0,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Color.fromRGBO(3, 54, 255, 1.0),
-                  borderRadius: BorderRadius.circular(8.0)
+              SizedBox(
+                height: 32.0,
               ),
-              child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0),
-            ),
+              SizedBox(
+                width: 100.0,
+                height: 100.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(3, 54, 255, 1.0),
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(
+                      colors: [
+                        Color.fromRGBO(7, 102, 255, 1.0),
+                        Color.fromRGBO(3, 54, 255, 1.0)
+                      ]
+                    )
+                  ),
+                  child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0),
+                ),
+              ),
+              Positioned(
+                right: 20.0,
+                top: 20.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 20.0),
+              ),
+              Positioned(
+                right: 20.0,
+                top: 120.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 16.0),
+              ),
+              Positioned(
+                right: 30.0,
+                top: 230.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 18.0),
+              ),
+              Positioned(
+                right: 90.0,
+                top: 20.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 16.0),
+              ),
+              Positioned(
+                right: 4.0,
+                top: -4.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 16.0),
+              ),
+            ],
           )
         ],
       ),
